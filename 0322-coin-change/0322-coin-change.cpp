98c3ebc33c4c8d61e8b5afbcc40882c83f914f3c
23211a05e4-ship-it <23211a05e4@gmail.com>
2026-03-12 12:17:06 +0530

    Time: 31 ms (56.57%), Space: 18.9 MB (58.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,amount+1);
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++){
-            for(int c : coins){
-                if(i-c >= 0) dp[i] = min(dp[i],1+dp[i-c]);
+    int sol(vector<int>&v,vector<int>&dp,int amt){
+        if(amt == 0) return 0;
+        if(dp[amt]!=INT_MIN) return dp[amt];
+        int mini = INT_MAX;
+        for(int i = 0; i < v.size(); i++){
+            if(amt - v[i] >= 0){
+                int res = sol(v, dp, amt - v[i]);
+                if(res != INT_MAX)
+                    mini = min(mini, 1 + res);
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amt] = mini;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,INT_MIN);
+        dp[0] = 0;
+        int ans = sol(coins,dp,amount);
+        if(ans == INT_MAX) return -1;
+        return ans;
     }
 };
