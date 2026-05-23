328a95a3ac2f0265295a93d313ed36d3f04ca1d2
23211a05e4-ship-it <23211a05e4@gmail.com>
2026-05-23 11:36:37 +0530

    Time: 0 ms (100%), Space: 11.4 MB (18.08%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int cnt = 0, n = arr.size();
+        for(int i = 0; i < n; i++){
+            if(arr[i] > arr[(i+1) % n]) cnt++;
+        }
+        return cnt <= 1;
+    }
+};
